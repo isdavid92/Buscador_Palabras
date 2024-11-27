f00ddd7436a75a085a34841fc4cfa26558667480
ISRAEL AGUILAR <117057916+isdavid92@users.noreply.github.com>
2024-11-27 16:05:53 -0500

    listo el resultado

--- src/Components/Resultado/Resultado.jsx
+++ src/Components/Resultado/Resultado.jsx
@@ -11,12 +11,30 @@ const Resultado = () => {
     const cuadricula = () => {
         let contenido = matriz;
         let sopaDeLetras = [];
-        for (let i = 0; i < 196; i++) {
-            sopaDeLetras.push(
-                <div key={i} className={style.letra}>{contenido[i] || ""}</div>
-            )
+        for (let i = 0; i < 14; i++) {
+            let fila = contenido[i] || 14;
+            for (let e = 0; e < 14; e++) {
+                let letra = fila[e];
+                sopaDeLetras.push(
+                    <div key={`${i}-${e}`} className={style.letra}>{letra || ""}</div>
+                )
+            }
         };
         return sopaDeLetras
+    };
+
+    const renderPalabras = (ord) => {
+        if (ord == "no") {
+            // let prueba = ["hola"];
+            console.log("aqui");
+            return noEncontradas.map((element, index) => {
+                return <div key={index} className={style.palabraNo}>{element}</div>
+            })
+        } else if (ord == "si") {
+            return encontradas.map((element, index) => {
+                return <div key={index} className={style.palabraSi}>{element}</div>
+            })
+        }
     };
 
     useEffect(() => {
@@ -30,9 +48,19 @@ const Resultado = () => {
                 <div className={style.sopaDeLetras}>
                     {cuadricula()}
                 </div>
-                <div className={style.palabras}>
-                    <div className={style.encontradas}></div>
-                    <div className={style.noEncontradas}></div>
+                <div className={style.contPalabras}>
+                    <div className={style.noEncontradas}>
+                        <h5 className={style.h5no}>NO ENCONTRADAS</h5>
+                        <div className={style.palabras}>
+                            {renderPalabras("no")}
+                        </div>
+                    </div>
+                    <div className={style.encontradas}>
+                        <h5 className={style.h5en}>ENCONTRADAS</h5>
+                        <div className={style.palabras}>
+                            {renderPalabras("si")}
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={style.boton}>
